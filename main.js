da5d3ae4d2179824131713ab1e38381382fb2e1c
Basaam Anwar <109045924+fatist13@users.noreply.github.com>
2023-09-07 13:13:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -8,14 +8,19 @@ document.getElementById("btn").addEventListener("click", btnHandler);
 
 function btnHandler() {
   // Flip Coins
-  let randNum1 = Math.random();
-  let coin1;
-  if (randNum1 < 0.5) {
-    coin1 = "heads";
-  } else {
-    coin1 = "tails";
-  }
-
+  let coin1 = flipCoin();
+  let coin2 = flipCoin();
+  let coin3 = flipCoin();
+ 
   // Output Results
-  outputEl.innerHTML = `<img src="img/${coin1}.png">`;
+  outputEl.innerHTML = `<img src="img/${coin1}.png"> <img src="img/${coin2}.png"> <img src="img/${coin3}.png">`;
+}
+
+function flipCoin() {
+  let randNum = Math.random();
+  if (randNum < 0.5) {
+    return "heads";
+  } else {
+    return "tails";
+  }
 }
